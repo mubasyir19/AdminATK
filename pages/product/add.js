5649d5094c7b57679f987fcf15d0c70ca1810005
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2023-09-04 18:46:25 +0700

    Coding Add Product Page

--- pages/product/add.js
+++ pages/product/add.js
@@ -2,9 +2,39 @@ import Footer from '@/components/Molecules/Footer';
 import Navbar from '@/components/Molecules/Navbar';
 import Sidebar from '@/components/Molecules/Sidebar';
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 
 export default function Add() {
+  const API = process.env.NEXT_API;
+  const router = useRouter();
+
+  const [nama, setNama] = useState('');
+  const [deskripsi, setDeskripsi] = useState('');
+  const [harga, setHarga] = useState(0);
+  const [stok, setStok] = useState(0);
+  const [kategori, setKategori] = useState('');
+  const [foto, setFoto] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // handle submit
+      // .....(code here)
+
+      // Response Success
+      // .....(sweetalert)
+      router.push('/product');
+    } catch (error) {
+      // handle error
+      // .....(code here)
+
+      // Response Error
+      // .....(sweetalert)
+      router.push('/product');
+    }
+  };
+
   return (
     <>
       <div className='container-scroller'>
@@ -41,7 +71,7 @@ export default function Add() {
                 <div className='col-md-12 grid-margin'>
                   <div className='row'>
                     <div className='col-12 col-xl-8 mb-4 mb-xl-0'>
-                      <h3 className='font-weight-bold'>Products Page</h3>
+                      <h3 className='font-weight-bold'>Add Product Page</h3>
                       <h6 className='font-weight-normal mb-0'>Manage Your Products!</h6>
                     </div>
                     <div className='col-12 col-xl-4'>
@@ -50,15 +80,105 @@ export default function Add() {
                           <li className='breadcrumb-item'>
                             <Link href='/'>Dashboard</Link>
                           </li>
-                          <li className='breadcrumb-item active'>Product</li>
+                          <li className='breadcrumb-item'>
+                            <Link href='/product'>Product</Link>
+                          </li>
+                          <li className='breadcrumb-item active'>Add</li>
                         </ol>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <Footer />
+              <div className='row'>
+                <div className='col-12 grid-margin stretch-card'>
+                  <div className='card'>
+                    <div className='card-body'>
+                      <h4 className='card-title'>Add Your Product</h4>
+                      {/* <p className='card-description'>Basic form elements</p> */}
+                      <form className='forms-sample'>
+                        <div className='form-group'>
+                          <label for='exampleInputName1'>Name</label>
+                          <input
+                            type='text'
+                            className='form-control'
+                            id='exampleInputName1'
+                            placeholder='Name'
+                            onChange={(e) => setNama(e.target.value)}
+                            required
+                          />
+                        </div>
+                        <div className='form-group'>
+                          <label for='exampleTextarea1'>Description</label>
+                          <textarea
+                            className='form-control'
+                            id='exampleTextarea1'
+                            rows='4'
+                            placeholder='Description Product...'
+                            onChange={(e) => setDeskripsi(e.target.value)}
+                          ></textarea>
+                        </div>
+                        <div className='form-group'>
+                          <label for='harga'>Price</label>
+                          <input
+                            type='number'
+                            className='form-control'
+                            id='harga'
+                            placeholder='Price'
+                            onChange={(e) => setHarga(e.target.value)}
+                            required
+                          />
+                        </div>
+                        <div className='form-group'>
+                          <label for='stok'>Stock</label>
+                          <input
+                            type='number'
+                            className='form-control'
+                            id='stok'
+                            placeholder='Stock'
+                            onChange={(e) => setStok(e.target.value)}
+                            required
+                          />
+                        </div>
+                        <div className='form-group'>
+                          <label for='category'>Category Product</label>
+                          <select className='form-control' id='category' onChange={(e) => setKategori(e.target.value)}>
+                            <option>Pensil</option>
+                            <option>Buku</option>
+                          </select>
+                        </div>
+                        <div className='form-group'>
+                          <label>Photo Upload</label>
+                          <input type='file' name='img[]' className='file-upload-default' />
+                          <div className='input-group col-xs-12'>
+                            <input
+                              type='text'
+                              className='form-control file-upload-info'
+                              disabled
+                              placeholder='Upload Photo'
+                            />
+                            <span className='input-group-append'>
+                              <button
+                                className='file-upload-browse btn btn-primary'
+                                type='button'
+                                onChange={(e) => setFoto(e.target.value)}
+                              >
+                                Upload
+                              </button>
+                            </span>
+                          </div>
+                        </div>
+                        <button type='submit' className='btn btn-primary mr-2' onClick={handleSubmit}>
+                          Submit
+                        </button>
+                        <button className='btn btn-light'>Cancel</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+            <Footer />
           </div>
         </div>
       </div>
